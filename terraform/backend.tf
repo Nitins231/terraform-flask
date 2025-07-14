094d622fb0bfdbd891080a8edfa60b898ef20973
Nitin sharma <ns465626@gmail.com>
2025-07-14 19:30:21 +0000

    Removed terraform directory from repository only

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-# backend.tf
-terraform {
-  # backend "s3" {
-  #   bucket         = "your-s3-bucket-name"
-  #   key            = "terraform.tfstate"
-  #   region         = "ap-south-1"
-  #   dynamodb_table = "terraform-lock"
-  # }
-}
